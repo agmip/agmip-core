e1f9e9b1561f6db19985679f7508115eed2017f0
Christopher Villalobos <christopher.villalobos@gmail.com>
2012-07-10 00:13:44 -0400

    Major API change

--- src/main/java/org/agmip/util/MapUtil.java
+++ src/main/java/org/agmip/util/MapUtil.java
@@ -2,95 +2,137 @@ package org.agmip.util;
 
 import java.util.Arrays;
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import com.google.common.base.Optional;
 
 public class MapUtil {
+    public static class BucketEntry {
+        private LinkedHashMap<String, String> values = new LinkedHashMap();
+        private ArrayList<LinkedHashMap<String, String>> dataList = new ArrayList();
+        private ArrayList<String> subBucketEntriesList = new ArrayList();
+        private LinkedHashMap<String, Object> subBucketEntries = new LinkedHashMap();
 
-    public static <K,V> V getValueOr(Map<K,V> m, K key, V orValue ) {
-        Optional<V> opt = Optional.fromNullable(m.get(key));
-        return opt.or(orValue);
-    }
+        public BucketEntry(LinkedHashMap<String, Object> m) {
+            for(Map.Entry<String, Object> entry : m.entrySet()) {
+                String key = entry.getKey();
+                Object value = entry.getValue();
 
-    public static <K,V> List<V> rawGetValuesOr(List<Map<K,V>> l, K key, V orValue) {
-        ArrayList<V> acc = new ArrayList<V>();
-        for(Map<K,V> m : l) {
-            acc.add(getValueOr(m, key, orValue));
-        }
-        return acc;
-    }
-
-    public static <K,V> List<V> getValuesOr(List<Map<K,V>> l, K key, V orValue, boolean uniqueOnly) {
-        ArrayList<V> acc = new ArrayList<V>();
-        V stickyVal = orValue;
-
-        for(Map<K,V> m : l) {
-            if(acc.size() == 0) {
-                stickyVal = getValueOr(m, key, orValue);
-                acc.add(stickyVal);
-            } else {
-                acc.add(getValueOr(m, key, stickyVal));
+                if(key.equals("data")) {
+                    this.dataList = (ArrayList<LinkedHashMap<String, String>>) value;
+                } else if ( value instanceof Map ) {
+                    this.subBucketEntriesList.add(key);
+                    this.subBucketEntries.put(key, value);
+                } else {
+                    values.put(key, (String) value);
+                }
             }
         }
-        if(uniqueOnly) {
-            return getUniqueValues(acc);
-        } else {
+
+        public BucketEntry() {}
+
+        public LinkedHashMap<String, String> getValues() {
+            return values;
+        }
+
+        public ArrayList<LinkedHashMap<String, String>> getDataList() {
+            return dataList;
+        }
+
+        public ArrayList<String> getSubBucketEntries() {
+            return subBucketEntriesList;
+        }
+
+        public BucketEntry getSubBucketEntry(String key) {
+            return (BucketEntry) getObjectOr(subBucketEntries, key, new BucketEntry());
+        }
+
+        public ArrayList<LinkedHashMap<String, String>> parseDataList(Map<String, String> mapOrValues) {
+            ArrayList<LinkedHashMap<String, String>> acc = new ArrayList();
+            LinkedHashMap<String, String> stickyMap = new LinkedHashMap();
+            for(Map<String, String> sourceMap : dataList) {
+                if( acc.size() == 0 ) {
+                    for(Map.Entry<String, String> e : mapOrValues.entrySet()) {
+                        stickyMap.put(e.getKey(), getObjectOr(sourceMap, e.getKey(), e.getValue()));
+                        acc.add(stickyMap);
+                    }
+                } else {
+                    LinkedHashMap<String, String> mergedMap = new LinkedHashMap();
+                    for(Map.Entry<String, String> e : stickyMap.entrySet()) {
+                        String val = getObjectOr(sourceMap, e.getKey(), e.getValue());
+                        mergedMap.put(e.getKey(), ( val.equals("") ? mapOrValues.get(e.getKey()) : val ));
+                        acc.add(mergedMap);
+                    }
+                }
+            }
             return acc;
         }
     }
 
-    public static <K,V> List<V> getValuesOr(List<Map<K,V>> l, K key, V orValue) {
-        return getValuesOr(l,key,orValue,false);
+    public static <K,V> V getObjectOr(Map<K,V> m, K key, V orValue ) {
+        Optional<V> opt = Optional.fromNullable(m.get(key));
+        return opt.or(orValue);
     }
 
-    private static <V> List<V> getUniqueValues(List<V> l) {
-        ArrayList<V> acc = new ArrayList<V>();
-        for(V value : l) {
-            if( ! acc.contains(value) ) {
-                acc.add(value);
+    /**
+     * Returns a list of all buckets in the provided Map.
+     */ 
+    public static ArrayList<String> listBucketNames(Map<String, Object> m) {
+        ArrayList<String> acc = new ArrayList<String>();
+        for(Map.Entry<String, Object> entry : m.entrySet()) {
+            if( isValidBucket(entry.getValue()) ) {
+                acc.add(entry.getKey());
             }
         }
         return acc;
     }
 
-    public static <K,V> List<Map<K,V>> rawExtractFromList(List<Map<K,V>> l, K[] keys, V orValue) {
-        ArrayList<Map<K,V>> acc = new ArrayList<Map<K,V>>();
-
-        for(Map<K,V> m : l) {
-            acc.add(rawExtract(m, keys, orValue));
+    public static ArrayList<BucketEntry> getBucket(Map<String, Object> m, String key) {
+        ArrayList<BucketEntry> acc = new ArrayList();
+        Object source = getObjectOr(m, key, new ArrayList());
+        if( isValidBucket(source) ) {
+            for(LinkedHashMap<String, Object> map : (ArrayList<LinkedHashMap<String,Object>>) source) {
+                acc.add(new BucketEntry(map));
+            }
         }
         return acc;
     }
 
-    public static <K,V> List<Map<K,V>> extractFromList(List<Map<K,V>> l, K[] keys, V orValue) {
-        ArrayList<Map<K,V>> acc = new ArrayList<Map<K,V>>();
-        Map<K,V> stickyVals = new LinkedHashMap<K,V>();
-        ArrayList<K> ks = new ArrayList<K>(Arrays.asList(keys));
+    public static LinkedHashMap<String, String> getGlobalValues(Map<String, Object> m) {
+        LinkedHashMap<String, String> globals = new LinkedHashMap<String, String>();
+        for(Map.Entry<String, Object> entry : m.entrySet()) {
+            if(entry.getValue() instanceof String) {
+                globals.put(entry.getKey(), (String) entry.getValue());
+            }
+        }
+        return globals;
+    }
 
-        for(Map<K,V> m : l) {
-            if(acc.size() == 0) {
-                stickyVals = rawExtract(m, keys, orValue);
-                acc.add(stickyVals);
+    public static String getValueOr(Map<String, String> m, String key, String orValue) {
+        return getObjectOr(m, key, orValue);
+    }
+
+
+    /**
+     * Check to see if the passed Object is a valid Bucket
+     */
+    private static boolean isValidBucket(Object test) {
+        if( test instanceof List ) {
+            if( ((List) test).size() == 0 ) {
+                return false;
             } else {
-                Map<K,V> newVal = new LinkedHashMap<K,V>();
-                for(K key: ks) {
-                    newVal.put(key, getValueOr(m, key, getValueOr(stickyVals, key, orValue)));
+                if( ((List) test).get(0) instanceof Map ) {
+                    return true;
+                } else {
+                    return false;
                 }
-                acc.add(newVal);
             }
+        } else if ( test instanceof Map ) {
+            return true;
+        } else {
+            return false;
         }
-        return acc;
-    }
-
-    public static <K,V> Map<K,V> rawExtract(Map<K,V> m, K[] keys, V orValue) {
-        ArrayList<K> ks = new ArrayList<K>(Arrays.asList(keys));
-        Map<K,V> extract = new LinkedHashMap<K,V>();
-        for(K key : ks) {
-            extract.put(key, getValueOr(m, key, orValue));
-        }
-        return extract;
     }
 }
