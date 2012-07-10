1d34aaddc30f4b8a62cf360b3cbb328b9407b9c6
Christopher Villalobos <christopher.villalobos@gmail.com>
2012-07-10 15:05:31 -0400

    Added NOT_NULL serialization to Jackson

--- src/main/java/org/agmip/util/JSONAdapter.java
+++ src/main/java/org/agmip/util/JSONAdapter.java
@@ -4,15 +4,20 @@ package org.agmip.util;
  * Simple JSON converter using Jackson
  */
 
-import java.util.LinkedHashMap;
 import java.io.IOException;
 import org.codehaus.jackson.map.ObjectMapper;
+import org.agmip.core.types.Experiment;
 
 public class JSONAdapter {
-    private static final ObjectMapper mapper = new ObjectMapper();
+    private static final ObjectMapper mapper;
 
-    public static LinkedHashMap fromJSON(String json) throws IOException {
-        return mapper.readValue(json, LinkedHashMap.class);
+    public JSONAdapter() {
+        mapper = new ObjectMapper();
+        mapper.setSerializationInclusion(JsonSerialize.Inclusion.NOT_NULL);
+    }
+
+    public static Experiment fromJSON(String json) throws IOException {
+        return mapper.readValue(json, Experiment.class);
     } 
 
     public static String toJSON(Object obj) throws IOException {
